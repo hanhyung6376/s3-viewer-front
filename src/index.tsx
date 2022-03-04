bb3ede825957a41ec25269f0668a91d848a7b42f
hanhyung6376 <hanhyung6376@naver.com>
2022-03-04 23:13:53 +0900

    FIRST COMMIT

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,18 @@
-import React from 'react';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import { RecoilRoot } from 'recoil';
+
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+const queryClient = new QueryClient();
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <RecoilRoot>
+        <QueryClientProvider client={queryClient}>
+            <App />
+        </QueryClientProvider>
+    </RecoilRoot>,
+    document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
