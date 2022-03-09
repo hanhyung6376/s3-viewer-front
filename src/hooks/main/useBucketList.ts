87d5cf930646070cb7a2690014e379d3462a1bce
leo <hanhyung6376@naver.com>
2022-03-09 17:19:52 +0900

    FIX: test

---
+++ src/hooks/main/useBucketList.ts
@@ -0,0 +1,9 @@
+import { useQuery } from 'react-query'
+import { bucketList } from 'lib/api/s3'
+
+const useBucketList = () => {
+    const access_token =
+
+}
+
+export default useBucketList;
--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,1 +1,7 @@
 /// <reference types="react-scripts" />
+declare namespace NodeJs {
+    interface ProcessEnv {
+        NODE_ENV: 'dev' | 'prod';
+        REACT_APP_BACKEND: string;
+    }
+}
